9971194f766ff0a3dfd8866e24694fc51a0b73a7
JulioC05 <skjulio05@gmail.com>
2020-11-13 15:44:05 -0500

    Template admin

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+
+//Template Admin
+Route::get('/admin', 'AdminController@dashboard');
+//Agregar-Template Admin
+Route::get('/addcategory', 'AdminController@addcategory');
+Route::get('/addproduct', 'ProductController@addproduct');
+Route::get('/addslider', 'SliderController@addslider');
+
+//Listar-Template Admin
+Route::get('/products', 'ProductController@products');
+Route::get('/categories', 'AdminController@categories');
+Route::get('/sliders', 'SliderController@sliders');
+Route::get('/orders', 'AdminController@orders');
