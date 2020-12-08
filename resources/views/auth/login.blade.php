84c473db89ed6ec3e38242b6dba234181fb23986
Stephanopb <40832730+Stephanopb@users.noreply.github.com>
2020-12-08 16:13:58 -0500

    LoginAdmin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,16 +1,27 @@
 @extends('layouts.app')
+@section('title')
+	LOGIN
+	
+@endsection
 
 @section('content')
-<div class="container">
+<br>
+<br>
+<br>
+<br>
+<br>
+<div class="container" >
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
-
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
+                        <br>
+                        <br>
+                        <br>
+                        
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
@@ -24,7 +35,9 @@
                                 @enderror
                             </div>
                         </div>
-
+                        <br>
+                        
+                        
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
@@ -38,7 +51,7 @@
                                 @enderror
                             </div>
                         </div>
-
+                        <br>
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
@@ -50,13 +63,13 @@
                                 </div>
                             </div>
                         </div>
-
+                       
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
-
+                                
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,12 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+<br>
+<br>
+<br>
+<br>
+<br>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
-
+                <br>
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
@@ -24,7 +29,7 @@
                                 @enderror
                             </div>
                         </div>
-
+                        <br>
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
@@ -38,7 +43,7 @@
                                 @enderror
                             </div>
                         </div>
-
+                        <br>
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
@@ -52,7 +57,7 @@
                                 @enderror
                             </div>
                         </div>
-
+                        <br>
                         <div class="form-group row">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
@@ -60,7 +65,7 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
-
+                        <br>
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
